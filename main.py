93f3feaba0d98b5296572ca6fb119a4bf64d32fc
safuente <santiagoafu@ext.inditex.com>
2025-04-21 15:22:24 +0200

    Added file management endpoints

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from auth import authentication
 from db import models
 from db.database import engine
 from exceptions import StoryException
-from router import blog_get, blog_post, user, article, product
+from router import blog_get, blog_post, user, article, product, file
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(blog_get.router)
@@ -15,6 +16,7 @@ app.include_router(blog_post.router)
 app.include_router(user.router)
 app.include_router(product.router)
 app.include_router(authentication.router)
+app.include_router(file.router)
 
 
 @app.get('/hello')
@@ -48,6 +50,8 @@ app.add_middleware(
     allow_headers=['*']
 
 )
+
+app.mount("/files", StaticFiles(directory="files"), name='files')
 
 
 
